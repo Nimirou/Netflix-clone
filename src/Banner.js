738f87ad3bb48dccaefddca13d8df087d4704802
Honza Klimeš <johny.klimes@seznam.cz>
2023-02-12 01:01:33 +0100

    feat: truncate

--- src/Banner.js
+++ src/Banner.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./Banner.css";
 
 function Banner() {
+  function truncate(string, n) {
+    return string?.length > n ? string.substring(0, n - 1) + "..." : string;
+  }
   return (
     <header
       className="banner"
@@ -17,7 +20,12 @@ function Banner() {
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">TEST Description</h1>
+        <h1 className="banner__description">
+          {truncate(
+            `Test extremně dlouheho textu a toho ze se to neposere Test extremně dlouheho textu a toho ze se to neposere Test extremně dlouheho textu a toho ze se to neposere`,
+            150
+          )}
+        </h1>
       </div>
       <div className="banner--fadeBottom" />
     </header>
