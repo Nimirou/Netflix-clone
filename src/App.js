36e39494c8377157e6cf7df61fb25140f972dd8e
Honza Klimeš <johny.klimes@seznam.cz>
2023-02-14 23:25:24 +0100

    feat: Router and firebase addition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import HomeScreen from "./HomeScreen";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        <Routes>
+          <Route path="/login"></Route>
+          <Route path="/" element={<HomeScreen />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
