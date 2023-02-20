8e81cc21390b9fb8d9359224cae28e9ea9332bab
Honza Klimeš <johny.klimes@seznam.cz>
2023-02-21 00:21:11 +0100

    feat: full css for sign in page

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -3,10 +3,16 @@ import "./SignInScreen.css";
 
 function SignInScreen() {
   return (
-    <div className="signUpScreen">
-      SignInScreen
+    <div className="signupScreen">
       <form>
-        <h1>Sign In</h1>
+        <h1 className="signupScreen__title">Sign In</h1>
+        <input placeholder="Email" type="email" />
+        <input placeholder="Password" type="password"></input>
+        <button type="submit">Sign In</button>
+        <h4>
+          <span className="signupScreenGrey">New to NETFLIX?</span>{" "}
+          <span className="signupScreen__link">Sign Up now.</span>
+        </h4>
       </form>
     </div>
   );
