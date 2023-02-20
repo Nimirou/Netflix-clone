a6efd1b010e012c7129934f000b1d29f3b621fd3
Honza Klimeš <johny.klimes@seznam.cz>
2023-02-21 00:07:19 +0100

    feat: sign In screen

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="app">
       <Router>
-        {user ? (
+        {!user ? (
           <LoginScreen></LoginScreen>
         ) : (
           <Routes>
---
+++ src/screens/SignInScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./SignInScreen.css";
+
+function SignInScreen() {
+  return (
+    <div className="signUpScreen">
+      SignInScreen
+      <form>
+        <h1>Sign In</h1>
+      </form>
+    </div>
+  );
+}
+
+export default SignInScreen;
